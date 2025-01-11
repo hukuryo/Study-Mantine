2488686e97c4755f329377f3ad22e8e471f35b6a
hukuryo <ryohei.1998.05.09@gmail.com>
2025-01-11 22:58:01 +0900

    ✨

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  experimental: {
+    dynamicIO: true,
+    reactCompiler: {
+      compilationMode: "annotation",
+    },
+  },
 };
 
 export default nextConfig;
